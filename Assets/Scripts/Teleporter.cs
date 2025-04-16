d1d9161e2781762924fc8654faf28bbcd655bfa4
Vaidotas <blangas2000@yahoo.com>
2025-04-16 13:52:39 +0200

    Item highlight Done and start of portals

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script from Creating a Portal System in Unity https://medium.com/@tmaurodot/creating-a-portal-system-in-unity-f25954537c00
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Transform targetTeleportLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
